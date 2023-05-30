f0cdce82d7072367f66e2079c3b3b0095ad3470b
vinicius <vinicius.lima.cordeiro@gmail.com>
2023-05-30 11:22:15 +0200

    up

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,5 @@ from .model import (dRdt, set_one_ring_connectivity,  set_external_ring_coupling
                     create_ring)
 from .integration import EULER, RK4, config_simulation
 from .utils import batch, phi, gaussian, create_square_kernel
-from .setParams import get_params_rate_model, get_params_spiking_model
+from .setParams import get_params_rate_model
 from .infodyn.conn_pid import conn_pid
